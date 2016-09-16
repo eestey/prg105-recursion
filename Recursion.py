92ef7a8b15025c54def29909448884007a6746bf
eestey <eestey@students.mchenry.edu>
2016-09-15 19:14:57 -0500

    Add files via upload

---
+++ Recursion.py
@@ -0,0 +1,7 @@
+def x(num_beers):
+    if x > 0:
+        print x, "bottles of beer on the wall,", x, ",bottles of beer."
+        print ("if one of those bottles should happen to fall")
+        print x - 1, " bottles of beer on the wall."
+
+
