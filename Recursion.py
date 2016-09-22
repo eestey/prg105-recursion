f55c30386933b5c371d6e8478bda0fc6cef1e499
eestey <eestey@students.mchenry.edu>
2016-09-21 19:57:26 -0500

    Delete Recursion.py

--- Recursion.py
+++
@@ -1,7 +0,0 @@
-def x(num_beers):
-    if x > 0:
-        print x, "bottles of beer on the wall,", x, ",bottles of beer."
-        print ("if one of those bottles should happen to fall")
-        print x - 1, " bottles of beer on the wall."
-
-
